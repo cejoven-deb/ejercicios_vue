62a5ff92aef7d9740be33a5647e8e8589362649c
cejoven-deb <carlosvivax77@hotmail.com>
2022-07-02 00:16:28 -0500

    nueva app

---
+++ componentes/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <h1>
+    componentes
+  </h1>
+  <card />
+  
+</template>
+
+<script>
+ 
+import card from './components/card.vue';
+export default {
+ components: {card}
+}
+</script>
---
+++ componentes/src/components/card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+       <img src="https://cdn.svgporn.com/logos/ibm.svg?response-content-disposition=attachment%3Bfilename%3Dibm.svg" alt="vue.js">
+       <h2>IBM</h2>
+       <a href="https://cdn.svgporn.com/logos/ibm.svg?response-content-disposition=attachment%3Bfilename%3Dibm.svg"
+       target="_blank">
+        Descargar
+       </a> 
+    </div>
+</template>
+<style scoped></style>
+
